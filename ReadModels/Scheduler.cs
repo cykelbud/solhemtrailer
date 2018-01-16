d61dcf1869f7d05918c11f7752888304d8c370fa
Anders Ancker <andersancker@hotmail.se>
2018-01-16 20:06:08 +0100

    1

--- ReadModels/Scheduler.cs
+++ ReadModels/Scheduler.cs
@@ -19,21 +19,21 @@ namespace ReadModels
             _bookings = _bookings.Where(b => b.Start.Day >= DateTime.Now.Day).ToList();
             var slotsPerDay = 5;
             var slotLength = 3;
-            var startTime = 8;
+            var startHrs = 8;
             // generate schedule
             var scheduleSlots = new List<ScheduleSlot>();
             for (var day = startDate; day < endDate; day = day.AddDays(1))
             {
                 for (int slot = 1; slot <= slotsPerDay; slot++)
                 {
-                    var startTime = 
-
+                    var hrs = startHrs + slot * slotLength;
+                    var startTime = day.AddHours(hrs);
                     var scheduleSlot = new ScheduleSlot()
                     {
                         BookingId = day.Ticks, // startticks
                         TrailerId = trailerId,
-                        StartTime = day,
-                        EndTime = day.AddDays(1).AddTicks(-1),
+                        StartTime = startTime,
+                        EndTime = startTime.AddHours(slotLength).AddTicks(-1),
                         IsAvaliable = true,
                     };
                     scheduleSlots.Add(scheduleSlot);
@@ -44,8 +44,8 @@ namespace ReadModels
             scheduleSlots.ForEach(s =>
             {
                 if (_bookings.Any(b =>
-                    (b.Start.Ticks > s.StartTime.Ticks && b.Start.Ticks < s.EndTime.Ticks) ||
-                    (b.End.Ticks > s.StartTime.Ticks && b.End.Ticks < s.EndTime.Ticks)
+                    (b.Start.Ticks >= s.StartTime.Ticks && b.Start.Ticks <= s.EndTime.Ticks) ||
+                    (b.End.Ticks >= s.StartTime.Ticks && b.End.Ticks <= s.EndTime.Ticks)
                 ))
                 {
                     s.IsAvaliable = false;
