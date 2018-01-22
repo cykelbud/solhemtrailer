74ffa7d8b8df9e6a8bcddcc757c30d266c53ba1e
Anders <anders.ancker@marginalen.se>
2018-01-22 10:42:32 +0100

    sms notifyer

---
+++ ReadModels/SmsNotifyer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core;
+using Events;
+
+namespace ReadModels
+{
+    public class SmsNotifyer:
+        ISubscribeTo<TrailerBookedEvent>,
+        ISubscribeTo<TrailerBookingCanceledEvent>
+    {
+        public void Handle(TrailerBookedEvent @event)
+        {
+            // skicka sms bokning
+        }
+
+        public void Handle(TrailerBookingCanceledEvent @event)
+        {
+            // skicka sms avbokning
+        }
+    }
+}
