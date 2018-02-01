fd350186ab07f4b079e05088e8a56e31da7389de
Anders Ancker <andersancker@hotmail.se>
2018-02-01 19:37:49 +0100

    v y

--- WebApp/ClientApp/app/components/trailer/trailer.ts
+++ WebApp/ClientApp/app/components/trailer/trailer.ts
@@ -2,7 +2,14 @@ import { autoinject, bindable } from 'aurelia-framework';
 import { Config, Rest } from 'aurelia-api';
 import { HttpClient } from 'aurelia-fetch-client';
 import * as moment from 'moment';
+import * as $ from "jquery";
 
+
+export enum Step {
+    Select = 0,
+    Book = 1,
+    Done = 2
+}
 
 @autoinject
 export class Trailer {
@@ -10,27 +17,126 @@ export class Trailer {
     api: Rest;
     slots: Array<ISlot[]>;
     bookings: IBooking[];
-    scheduleDateDisplayText : string = '2018-01-31 till 2018-02-05';
+    @bindable scheduleDateDisplayText: string;
+    currentStep : Step;
+    selectedSlot : ISlot;
 
     constructor(config: Config) {
         this.api = config.getEndpoint('trailer');
+        this.currentStep = Step.Select;
     }
 
     async attached() {
 
+        let day = moment().isoWeekday() - 1;
+        let dateFirstDayOfWeek = moment().add(-day, 'days').format('YYYY-MM-DD');
+        let dateLastDayOfWeek = moment().add(7 - day, 'days').format('YYYY-MM-DD');
 
-        moment
+        this.scheduleDateDisplayText = dateFirstDayOfWeek + ' till ' + dateLastDayOfWeek;
 
         let search: IScheduleCriteria =
             {
-                StartDate: '2018-01-21',
-                EndDate: '2018-01-28'
+                StartDate: dateFirstDayOfWeek,
+                EndDate: dateLastDayOfWeek
             };
         this.slots = await this.api.find('slot', search);
         await this.getAllBookings();
+
+
+        $('.f1 fieldset:first').fadeIn('slow');
+
+        // next step
+        // $('.f1 .btn-next').on('click', function () {
+        //     var parent_fieldset = $(this).parents('fieldset');
+        //     var next_step = true;
+        //     // navigation steps / progress steps
+        //     var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+        //     var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+
+        //     // fields validation
+        //     parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function () {
+        //         if ($(this).val() == "") {
+        //             $(this).addClass('input-error');
+        //             next_step = false;
+        //         }
+        //         else {
+        //             $(this).removeClass('input-error');
+        //         }
+        //     });
+        //     // fields validation
+
+        //     if (next_step) {
+        //         parent_fieldset.fadeOut(400, function () {
+        //             // change icons
+        //             current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+        //             // progress bar
+        //             bar_progress(progress_line, 'right');
+        //             // show next step
+        //             $(this).next().fadeIn();
+        //             // scroll window to beginning of the form
+        //             scroll_to_class($('.f1'), 20);
+        //         });
+        //     }
+
+        // });
+
+        // previous step
+        // $('.f1 .btn-previous').on('click', function () {
+        //     // navigation steps / progress steps
+        //     var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+        //     var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+
+        //     $(this).parents('fieldset').fadeOut(400, function () {
+        //         // change icons
+        //         current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
+        //         // progress bar
+        //         bar_progress(progress_line, 'left');
+        //         // show previous step
+        //         $(this).prev().fadeIn();
+        //         // scroll window to beginning of the form
+        //         scroll_to_class($('.f1'), 20);
+        //     });
+        // });
+
+
+        function scroll_to_class(element_class: JQuery<HTMLElement>, removed_height: any) {
+            let offset = element_class.offset();
+
+            if (!offset) {
+                return;
+            }
+
+            var scroll_to = offset.top - removed_height;
+            if ($(window).scrollTop() != scroll_to) {
+                $('html, body').stop().animate({ scrollTop: scroll_to }, 0);
+            }
+        }
+
+        function bar_progress(progress_line_object: any, direction: any) {
+            var number_of_steps = progress_line_object.data('number-of-steps');
+            var now_value = progress_line_object.data('now-value');
+            var new_value = 0;
+            if (direction == 'right') {
+                new_value = now_value + (100 / number_of_steps);
+            }
+            else if (direction == 'left') {
+                new_value = now_value - (100 / number_of_steps);
+            }
+            progress_line_object.attr('style', 'width: ' + new_value + '%;').data('now-value', new_value);
+        }
     }
 
-    public async select(slot: ISlot) {
+    public select(slot: ISlot) {
+        if(this.selectedSlot){
+            this.selectedSlot.IsSelected = false;
+        }
+
+        slot.IsSelected = true;
+        this.selectedSlot = slot;
+    }
+
+    public async book(){
+        let slot = this.selectedSlot;
         let request: IBookRequest = {
             Email: "mail@mail.com",
             Phone: "1234567890",
@@ -40,7 +146,38 @@ export class Trailer {
         await this.api.post('booking', request);
         slot.IsAvailable = false;
         await this.getAllBookings();
+
+        this.next();
     }
+
+    public next(): void {
+        let step = this.currentStep;
+        switch(step){
+            case Step.Select:
+                this.currentStep = Step.Book;
+            case Step.Book:
+                this.currentStep = Step.Done;
+            case Step.Done:
+                this.currentStep = Step.Select;
+            default:
+                this.currentStep = Step.Select;
+        }
+    }
+
+    public previous(): void{
+        let step = this.currentStep;
+        switch(step){
+            case Step.Select:
+                this.currentStep = Step.Done;
+            case Step.Book:
+                this.currentStep = Step.Select;
+            case Step.Done:
+                this.currentStep = Step.Book;
+            default:
+                this.currentStep = Step.Select;
+        }
+    }
+
 
     public async getAllBookings() {
         this.bookings = await this.api.find('booking');
@@ -55,6 +192,9 @@ export class Trailer {
     public getJson(obj: any) {
         return JSON.stringify(obj);
     }
+
+
+
 
 
 }
@@ -73,7 +213,7 @@ interface ISlot {
     EndTime: number;
     IsAvailable: boolean;
     Date: string;
-    IsSelected : boolean;
+    IsSelected: boolean;
 }
 
 interface IBooking {
--- WebApp/ClientApp/boot.ts
+++ WebApp/ClientApp/boot.ts
@@ -17,7 +17,6 @@ export function configure(aurelia: Aurelia) {
 
         // // Add this:
         aurelia.use.plugin(PLATFORM.moduleName('aurelia-api'), (config : Config) => {
-            config.registerEndpoint('events', 'https://sagenda-sagenda-v1.p.mashape.com/Events/', {headers: {"X-Mashape-Key": 'QFbn17sJgwmshpiSjrunYHfAqUqyp1281gfjsnoMU5wXfWXCNV'}});
             config.registerEndpoint('trailer', 'api/trailer/');
         });
 
@@ -25,11 +24,6 @@ export function configure(aurelia: Aurelia) {
     if (IS_DEV_BUILD) {
         aurelia.use.developmentLogging();
     }
-
-    new HttpClient().configure(config => {
-        const baseUrl = document.getElementsByTagName('base')[0].href;
-        config.withBaseUrl(baseUrl);
-    });
 
     aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app/components/app/app')));
 }
